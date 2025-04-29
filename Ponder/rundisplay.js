368d88af83321ce0baf47ecc67c0e4626d994f7a
Quadeatschool <quadehafen@byui.edu>
2025-04-29 09:04:32 -0600

    4/29

---
+++ Ponder/rundisplay.js
@@ -0,0 +1,7 @@
+const mydino = 15;
+const buddydino = 14;
+let total = mydino + buddydino;
+
+document.getElementById("mydinoelement").textContent = mydino;
+document.getElementById("buddydinoelement").textContent = buddydino;
+document.getElementById("totaldinoelement").textContent = total;
