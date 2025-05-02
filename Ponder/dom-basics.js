4e7f4370d19e729fd9059c25b47a5190c6ee3ec3
Quadeatschool <quadehafen@byui.edu>
2025-05-02 10:30:29 -0600

    java

--- Ponder/dom-basics.js
+++ Ponder/dom-basics.js
@@ -1,3 +1,17 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
+
+
+const aimage = document.createElement("img");
+aimage.setAttribute("src", "5stages.png");
+aimage.setAttribute("alt", "a screenshot");
+document.body.appendChild(aimage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const sec3 = document.createElement("section");
+sec3.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(sec3);
