3f1a83709071beaf79d9663f80100698ba5177f4
Quadeatschool <quadehafen@byui.edu>
2025-05-15 09:23:46 -0600

    3 c

--- ponder 4/events.js
+++ ponder 4/events.js
@@ -2,19 +2,30 @@
 
 function newTask() {
     // get the list element from the DOM
+    const listElement = document.querySelector("#todoList");
+
+   
+    
+    
     // get the value entered into the #todo input
+    const task = document.querySelector('#todo').value;
+
     // render out the list 
     listElement.innerHTML += `
-      <li> ${task}
+      <li>  ${task}
         <div>
           <span data-function="delete">❎</span>
           <span data-function="complete">✅</span>
         </div>
-      </li>`
+      </li>
+      `;
+      
+      // inputtodo.value = '';
   }
   
   function manageTasks(e) {
     // using the event find the li element closest to what they clicked
+    console.log(e);
     const parent = e.target.closest("li");
       // did they click the delete or complete icon?
       if (e.target.getAttribute('data-function') === "delete") {
@@ -27,3 +38,7 @@ function newTask() {
   
   // Add your event listeners here
   // We need to attach listeners to the submit button and the list. Listen for a click, call the 'newTask' function on submit and call the 'manageTasks' function if either of the icons are clicked in the list of tasks.
+
+  document.querySelector("#submitTask").addEventListener('click',newTask);
+
+  document.querySelector("#todoList").addEventListener("click",manageTasks);
