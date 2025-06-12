e3eb545c222a69f1bfa9df31eb803ca7b9fde41f
Quadeatschool <quadehafen@byui.edu>
2025-06-12 10:59:29 -0600

    it is alive

--- ponder8/object.js
+++ ponder8/object.js
@@ -2,11 +2,37 @@
 const aCourse = {
   code: "CSE121b",
   name: "Javascript Language",
+   
 
   sections:[
     {sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
-    {sectionNum: 2, rromNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'sis A'} 
+    {sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'sis A'} 
 ],
+    enrollStudent: function(sectionNum)
+    {
+        const sectionIndex = this.sections.findIndex(section => section.sectionNum == sectionNum);
+        //If the section exists, then add 1 to
+        //  enrolled, and then call the renderSections function to show our change.
+
+        if (sectionIndex >= 0)
+    {
+        this.sections[sectionIndex].enrolled++;
+        OutPut(this.sections);
+
+    }
+    },
+
+    dropStudent: function(sectionNum) 
+    {
+        const sectionIndex = this.sections.findIndex(section => section.sectionNum == sectionNum);
+        if (sectionIndex >=0)
+        {
+            this.sections[sectionIndex].enrolled--;
+            OutPut(this.sections);
+        }
+    },
+    
+
 };
 
 function SetInfo(course)
@@ -26,7 +52,7 @@ function SectionDisplay(section)
     <td>${section.roomNum}</td>
     <td>${section.enrolled}</td>
     <td>${section.days}</td>
-    <<td>${section.instructor}</td>
+    <td>${section.instructor}</td>
     </tr>`
 }
 
@@ -36,5 +62,25 @@ function OutPut(sections)
     document.querySelector("#sections").innerHTML = html.join(" ");
 }
 
+
+
+// Add two event listeners to your code. 
+// One for the #enrollStudent button that will call out enrollStudent method, and another for the #dropStudent button.
+//  The event handler function in each case should grab the contents of the #sectionNumber input and pass it into the appropriate function.
+
+document.querySelector("#enrollStudent").addEventListener("click", function ()
+{
+    const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    aCourse.enrollStudent(sectionNum);
+});
+
+document.querySelector("#dropStudent").addEventListener("click", function()
+{
+    const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    aCourse.dropStudent(sectionNum);
+});
+
+
+SetInfo(aCourse);
 OutPut(aCourse.sections);
-SetInfo(sCourse);
+
