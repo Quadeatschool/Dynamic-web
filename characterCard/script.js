ea20a2413ed62218247d3aa2dfca24d53d0bc7bd
Quadeatschool <quadehafen@byui.edu>
2025-06-13 17:02:06 -0600

    sidiousa

--- characterCard/script.js
+++ characterCard/script.js
@@ -3,11 +3,12 @@ const character = {
       class: "Sith Space Witch",
       level: 5,
       health: 100,
-      image: 'C:\Users\knigh\Documents\Dynamic-web\characterCard\openart-image_v79GCcJX_1749748070816_raw.jpg',
+      image: 'openart-image_v79GCcJX_1749748070816_raw.jpg',
       attacked() {
         if (this.health >= 20) {
           this.level -= 1;
           this.health -= 20;
+          
         } else {
             alert('Character Died');
         }
@@ -15,12 +16,13 @@ const character = {
       levelUp() {
         this.level += 1;
         this.health += 20;
+        // CharacterRendering(this);
       }
     };
 
     function SetInfo(characters)
     {
-        const characterName = document.querySelector("#name");
+        const characterName = document.querySelector(".name");
         characterName.textContent = characters.name;
 
         const characterClass = document.querySelector("#class");
@@ -32,6 +34,31 @@ const character = {
         const characterHealth = document.querySelector("#health");
         characterHealth.textContent = characters.health;
 
-        
+        const characterImage = document.querySelector(".image");
+        characterImage.src = characters.image;
     }
 
+//--------------------------------------------
+
+    function CharacterRendering(characters)
+    {
+        document.querySelector(".name").innerHTML = `<p>${characters.name}<p>`;
+        document.querySelector("#class").innerText = characters.class;
+        document.querySelector("#level").innerText = characters.level;
+        document.querySelector("#health").innerText = characters.health;
+    }
+
+document.querySelector("#attacked").addEventListener("click", function()
+{
+    character.attacked();
+    CharacterRendering(character);
+});
+
+document.querySelector("#levelup").addEventListener("click", function()
+{
+    character.levelUp();
+    CharacterRendering(character);
+});
+
+SetInfo(character);
+CharacterRendering(character);
