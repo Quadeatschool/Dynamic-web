6474d4f2d12c59d670a876800e6aa13d94069ef6
Quadeatschool <quadehafen@byui.edu>
2025-07-07 14:33:26 -0600

    deploy 2

--- student-perk-archive/react-app/vite.config.js
+++ student-perk-archive/react-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/student-perk-archive/',
+  base: '/Dynamic-web/student-perk-archive/react-app/',
   plugins: [react()],
 })
